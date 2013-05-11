30f8dfb83f1cf1916831a732ad6e18c874812d97
rhenium <re4k@re4k.info>
2013-05-11 16:52:15 +0900

    add describe Favorite

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Favorite do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".from_hash" do
+    let(:hash) { {tweet_id: 123, user_id: 456} }
+    subject { Favorite.from_hash(hash) }
+    its(:tweet_id) { should be 123 }
+    its(:user_id) { should be 456 }
+  end
+
+  describe ".from_tweet_object" do
+    let(:user) { FactoryGirl.create(:user_1) }
+    let(:tweet) { FactoryGirl.create(:tweet, user: user) }
+    let(:tweet_object) { OpenStruct.new(
+      favoriters: [123, 456, 789, 1012, 345, 678],
+      id: tweet.id) }
+    subject { Favorite.from_tweet_object(tweet_object); tweet.favorites.map(&:user_id).sort }
+    its(:count) { should be 6 }
+    it { should eq tweet_object.favoriters.sort }
+  end
 end

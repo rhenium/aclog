b1c14b5b5bbcb8ed2aef996a166986220a7bf725
rhenium <re4k@re4k.info>
2013-05-11 17:20:44 +0900

    add describe Retweet

--- spec/models/retweet_spec.rb
+++ spec/models/retweet_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Retweet do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".from_hash" do
+    let(:hash) { {id: 890, tweet_id: 123, user_id: 456} }
+    subject { Retweet.from_hash(hash) }
+    its(:id) { should be 890 }
+    its(:tweet_id) { should be 123 }
+    its(:user_id) { should be 456 }
+  end
+
+  describe ".from_tweet_object" do
+    let(:user) { FactoryGirl.create(:user_1) }
+    let(:tweet) { FactoryGirl.create(:tweet, user: user) }
+    let(:tweet_object) { OpenStruct.new(
+      user: OpenStruct.new(user.attributes.update(profile_image_url_https: "abc")),
+      id: 890,
+      user_id: 123,
+      retweeted_status: OpenStruct.new(id: tweet.id)) }
+    subject { Retweet.from_tweet_object(tweet_object); tweet.retweets.map(&:user_id).sort }
+    its(:count) { should be 1 }
+    it { should eq [user.id] }
+  end
 end

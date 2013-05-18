08bf90fee8a4cdcaffef40fd59d3d737cc11074f
rhenium <re4k@re4k.info>
2013-05-18 22:25:05 +0900

    disable Haml ugly option

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,4 +1,5 @@
 Haml::Template.options[:attr_wrapper] = "\""
 Haml::Template.options[:format] = :xhtml
 Haml::Template.options[:mime_type] = "application/xhtml+xml"
+Haml::Template.options[:ugly] = false
 

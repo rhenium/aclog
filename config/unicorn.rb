69e63e999089e1913f294a422ea75adc96f49247
rhenium <re4k@re4k.info>
2013-05-24 19:34:27 +0900

    update unicorn.conf

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -16,9 +16,9 @@ before_fork do |server, worker|
   end
 
   old_pid = "#{server.config[:pid]}.oldbin"
-  unless old_pid == server.pid
+  if File.exists?(old_pid) && old_pid != server.pid
     begin
-      Process.kill :QUIT, File.read(old_pid).to_i
+      Process.kill(:QUIT, File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
@@ -31,7 +31,7 @@ after_fork do |server, worker|
 
   if defined?(ActiveSupport::Cache::DalliStore) && Rails.cache.is_a?(ActiveSupport::Cache::DalliStore)
     Rails.cache.reset
-    ObjectSpace.each_object(ActionDispatch::Session::DalliStore) { |obj| obj.reset }
+    ObjectSpace.each_object(ActionDispatch::Session::DalliStore) {|obj| obj.reset }
   end
 end
 

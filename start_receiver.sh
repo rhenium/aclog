ed1495ce932e8b9f52509229187e5dbbfe225369
Rhenium <rhenium@rhe.jp>
2013-08-22 08:34:24 +0900

    update starting script

---
+++ start_receiver.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export RAILS_ENV=production
+receiver="bundle exec rails runner script/start.rb"
+
+case "$1" in
+    start)
+        $receiver start
+        ;;
+    stop)
+        $receiver stop
+        ;;
+    restart)
+        $receiver restart
+        ;;
+    status)
+        $receiver status
+        ;;
+    *)
+        echo "usage: $0 {start|stop|restart|status}"
+        exit 1
+esac
+

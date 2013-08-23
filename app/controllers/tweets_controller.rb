119af6da7a6d7c8a4f614f4182ef6f8d7b657b81
Rhenium <rhenium@rhe.jp>
2013-08-23 10:20:49 +0900

    fix search

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -88,7 +88,7 @@ class TweetsController < ApplicationController
 
   def search
     @caption = "Search"
-    @tweets = Tweet.parse_query(params[:query].to_s || "").reacted.order_by_id.list(params, force_page: true)
+    @tweets = Tweet.parse_query(params[:q].to_s || "").reacted.not_protected.order_by_id.list(params, force_page: true)
     @tweets = @tweets.recent(7) unless @tweets.to_sql.include?("`tweets`.`id`")
   end
 

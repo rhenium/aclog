1080186863454a4de59aae8c0631f0631203397c
Rhenium <rhenium@rhe.jp>
2013-11-19 06:23:20 +0900

    change favoriters limit

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,18 +1,2 @@
 module TweetsHelper
-  def html_favoriters_limit
-    if params[:action] == "show"
-      if params[:full] == "true"
-        nil
-      else
-        Settings.tweets.users.count_lot
-      end
-    else
-      Settings.tweets.users.count_default
-    end
-  end
-
-  def html_favoriters_truncated?(tweet)
-    tr = html_favoriters_limit || Float::INFINITY
-    tr < tweet.favorites_count || tr < tweet.retweets_count
-  end
 end

1464f3580b432bb1b93eca06874cfbad2a0c37cd
Rhenium <rhenium@rhe.jp>
2013-11-19 22:23:38 +0900

    truncate tweet's favorites

--- app/helpers/tweets_helper.rb
+++ app/helpers/tweets_helper.rb
@@ -1,2 +1,9 @@
 module TweetsHelper
+  def favorites_truncate_count
+    params[:full] == "true" ? nil : Settings.tweets.favorites.truncate
+  end
+
+  def favorites_truncated?(tweet)
+    (favorites_truncate_count || Float::INFINITY) < [tweet.favorites_count, tweet.retweets_count].max
+  end
 end

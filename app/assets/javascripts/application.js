edfd74b8e73a25259a4d0647d5e80850b072c354
Rhenium <rhenium@rhe.jp>
2014-02-22 00:15:11 +0900

    fix top page: don't collapse navbar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,16 @@
 //= require_tree .
 
 $(function() {
+    // Layout: User jump
+    $("#jump_to_dropdown_toggle").click(function() {
+        setTimeout(function() { $("#jump_to_textbox").focus(); }, 0);
+    });
+    $("#jump_to_form").on("submit", function() {
+        $(this).attr("action", "/" + $("#jump_to_textbox").val());
+    });
+});
+
+$(function() {
     var controller = $("body").data("controller");
     var action = $("body").data("action");
 

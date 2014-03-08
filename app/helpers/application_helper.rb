1647b05cb2e8604d716dec4072c20ce9de3bfbaa
Rhenium <rhenium@rhe.jp>
2014-03-09 02:01:16 +0900

    fix profile_image load error handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,22 @@ module ApplicationHelper
     link_to name, options, html_options, &block
   end
 
+  def profile_image_tag(user, size = nil, options = {})
+    if size
+      url = user.__send__(:"profile_image_url_#{size}")
+    else
+      url = user.profile_image_url
+    end
+
+    capture_haml do
+      haml_tag("img.twitter-icon",
+               { src: url,
+                 alt: "@" + user.screen_name,
+                 onerror: "this.src = '#{image_path("profile_image_missing.png")}'"
+               }.merge(options))
+    end
+  end
+
   # utf8, form
   def utf8_enforcer_tag; raw "" end
 end

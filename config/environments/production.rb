ad103cccdfee1b3b5f43634016e50bc5afa12dec
Rhenium <rhenium@rhe.jp>
2014-03-18 23:21:40 +0900

    fix config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, pool_size: 5
+  config.cache_store = :dalli_store, { pool_size: 5 }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"

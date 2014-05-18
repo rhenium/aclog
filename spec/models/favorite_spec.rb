10792bebf3deb07026ef7533030a19aa2d9099d4
rhenium <rhenium@rhe.jp>
2014-05-18 20:15:58 +0900

    app: update spec of Favorite

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,38 +1,32 @@
 require "spec_helper"
 
-describe Retweet do
+describe Favorite do
   subject(:user) { create(:user) }
   subject(:tweet) { create(:tweet, user: user, favorites_count: 1, reactions_count: 1) }
   subject(:favoriter) { create(:user) }
   subject(:favorite) { create(:favorite, tweet: tweet, user: favoriter) }
 
-  describe "create_from_json" do
+  describe "create_bulk_from_json" do
     before do
-      @args = { source: {},
-                target_object: {} }
-      allow(Tweet).to receive(:create_from_json) { tweet }
-      allow(User).to receive(:create_from_json) { favoriter }
+      @args = [{ source: { id: favoriter.id },
+                 target_object: { id: tweet.id,
+                                  user: { id: user.id } } }]
     end
-    it "creates new favorite and increment favorites_count" do
+    it "creates new favorite" do
       favorite.destroy
-      old_favs = tweet.reload.favorites_count
-      ret = Favorite.create_from_json(@args)
-      expect(ret.tweet).to eq tweet
-      expect(ret.user).to eq favoriter
-      expect(tweet.reload.favorites_count).to be(old_favs + 1)
+      Favorite.create_bulk_from_json(@args)
+      f = Favorite.where(user: user, tweet: tweet)
+      expect(f).not_to be nil
     end
-    it "ignores ActiveRecord::RecordNotUnique and returns favorite" do
-      expect { Favorite.create_from_json(@args) }.to_not raise_error
-      expect(Favorite.create_from_json(@args).user).to eq favoriter
+    it "ignores errors if already exists" do
+      expect { Favorite.create_bulk_from_json(@args) }.not_to raise_error
     end
   end
 
-  describe "destroy_from_json" do
-    it "destorys the favorite and decrement favorites_count" do
-      old_favs = tweet.reload.favorites_count
-      Favorite.destroy_from_json(source: { id: favorite.user_id }, target_object: { id: favorite.tweet_id })
+  describe "delete_bulk_from_json" do
+    it "destorys the favorite" do
+      Favorite.delete_bulk_from_json([{ source: { id: favorite.user_id }, target_object: { id: favorite.tweet_id } }])
       expect { favorite.reload }.to raise_error ActiveRecord::RecordNotFound
-      expect(tweet.reload.favorites_count).to be(old_favs - 1)
     end
   end
 end

d2b3da45a3ceb6b9e997eb573fee8f3fdcb34f14
rhenium <rhenium@rhe.jp>
2014-05-18 20:16:09 +0900

    app: update spec of Retweet

--- spec/models/retweet_spec.rb
+++ spec/models/retweet_spec.rb
@@ -6,36 +6,28 @@ describe Retweet do
   subject(:retweeter) { create(:user) }
   subject(:retweet) { create(:retweet, tweet: tweet, user: retweeter) }
 
-  describe "create_from_json" do
+  describe "create_bulk_from_json" do
     before do
-      @args = { id: retweet.id,
-                user: {},
-                retweeted_status: {} }
+      @args = [{ id: retweet.id,
+                 user: { id: retweeter.id },
+                 retweeted_status: { id: tweet.id,
+                                     user: { id: user.id } } }]
     end
-    it "creates new retweet and increment retweets_count" do
-      allow(Tweet).to receive(:create_from_json) { tweet }
-      allow(User).to receive(:create_from_json) { retweeter }
-      old_rts = tweet.reload.retweets_count
-      ret = Retweet.create_from_json(@args.merge(id: retweet.id + 1))
-      expect(ret.tweet).to eq tweet
-      expect(ret.user).to eq retweeter
-      expect(tweet.reload.retweets_count).to be(old_rts + 1)
+    it "creates new retweet" do
+      Retweet.create_bulk_from_json(@args.merge(id: retweet.id + 1))
+      r = Retweet.where(user: user, tweet: tweet)
+      expect(ret.count).to be 2
+      expect(ret.order(id: :desc).first.id).to be retweet.id + 1
     end
-    it "ignores ActiveRecord::RecordNotUnique and returns retweet" do
-      user2 = create(:user)
-      allow(Tweet).to receive(:create_from_json) { tweet }
-      allow(User).to receive(:create_from_json) { user2 }
-      expect { Retweet.create_from_json(@args) }.to_not raise_error
-      expect(Retweet.create_from_json(@args).user).to eq retweeter
+    it "ignores errors if already exists" do
+      expect { Retweet.create_bulk_from_json(@args) }.not_to raise_error
     end
   end
 
-  describe "destroy_from_json" do
-    it "destorys the retweet and decrement retweets_count" do
-      old_rts = tweet.reload.retweets_count
-      Retweet.destroy_from_json(delete: { status: { id: retweet.id, user_id: retweeter.id } })
+  describe "delete_bulk_from_json" do
+    it "destorys the retweet" do
+      Retweet.delete_bulk_from_json(delete: { status: { id: retweet.id, user_id: retweeter.id } })
       expect { retweet.reload }.to raise_error ActiveRecord::RecordNotFound
-      expect(tweet.reload.retweets_count).to be(old_rts - 1)
     end
   end
 end

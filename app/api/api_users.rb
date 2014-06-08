daa66b13a0a2535076b3d98d8a5e0dee07226dd3
rhenium <rhenium@rhe.jp>
2014-06-09 05:48:58 +0900

    api: users/user_stats returns "registered"

--- app/api/api_users.rb
+++ app/api/api_users.rb
@@ -17,19 +17,19 @@ class ApiUsers < Grape::API
       end
     end
 
-    desc "Returns the stats of a user, specified by username or user ID.", example_params: { user_id: 280414022 }
+    desc "Returns the stats of a user, specified by username or user ID.", example_params: { id: 15926668 }
     params_user[]
     get "stats", rabl: "user_stats" do
       @user = user
     end
 
-    desc "Returns the list of the users who discovered the Tweets of a user, specified by username or user ID.", example_params: { user_id: 99008565 }
+    desc "Returns the list of the users who discovered the Tweets of a user, specified by username or user ID.", example_params: { id: 15926668 }
     params_user[]
     get "discovered_by", rabl: "user_discovered_by_and_users" do
       @result = user.count_discovered_by.sort_by {|user_id, count| -count }
     end
 
-    desc "Returns the list of the users discovered by a user, specified by username or user ID.", example_params: { screen_name: "cn" }
+    desc "Returns the list of the users discovered by a user, specified by username or user ID.", example_params: { screen_name: "toshi_a" }
     params_user[]
     get "discovered_users", rabl: "user_discovered_by_and_users" do
       @result = user.count_discovered_users.sort_by {|user_id, count| -count }
--- app/api/templates/user_stats.rabl
+++ app/api/templates/user_stats.rabl
@@ -2,3 +2,4 @@ object @user
 
 attributes :id
 node(:reactions_count) {|obj| obj.stats.reactions_count }
+node(:registered) {|obj| obj.stats.registered }

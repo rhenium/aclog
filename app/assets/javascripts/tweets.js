f55fcd3eb11022121468496ccbc76f022f27a13c
rhenium <rhenium@rhe.jp>
2014-07-14 05:43:47 +0900

    web: refactor views (use helper to render loading animation)

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,30 +1,27 @@
 Application.tweets = {
     _: function() {
-        if ($("#statuses")) {
-            var format_tweet = function(d) {
-                $("time", d).each(
-                    function() {
-                        $(this).text(new Date($(this).attr("datetime"))
-                                     .toLocaleString());
-                    });
-            };
-            format_tweet($(".statuses"));
+        var formatTweet = function(d) {
+            $("time", d).each(function() {
+                $(this).text(new Date($(this).attr("datetime"))
+                                .toLocaleString());
+            });
+        };
+        formatTweet($(".statuses"));
 
-            if ($("link[rel=next]")) {
-                $.autopager({
-                    content: $(".statuses"),
-                    link: $("link[rel=next]"),
-                    onStart: function() {
-                        $(".loading").show();
-                    },
-                    onReceived: function(obj) {
-                        format_tweet(obj);
-                    },
-                    onComplete: function() {
-                        $(".loading").hide();
-                    }
-                });
-            }
+        if ($("link[rel=next]") !== null) {
+            $.autopager({
+                content: $(".statuses"),
+                link: $("link[rel=next]"),
+                onStart: function() {
+                    $(".loading").show();
+                },
+                onReceived: function(obj) {
+                    formatTweet(obj);
+                },
+                onComplete: function() {
+                    $(".loading").hide();
+                }
+            });
         }
 
         $(".statuses").on("click", ".expand-responses-button", function() {

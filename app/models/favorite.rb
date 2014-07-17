01f1aba62a33cbac8c3055bbbc6dad63cbc008e3
rhenium <rhenium@rhe.jp>
2014-07-18 06:43:11 +0900

    doc: add YARD doc for Favorite and Retweet model

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,10 @@ class Favorite < ActiveRecord::Base
   belongs_to :tweet
   belongs_to :user
 
-  # This doesn't update Tweet#reactions_count.
+  # Registers favorite event in bulk from an array of Streaming API events.
+  # This method doesn't update Tweet#reactions_count.
+  #
+  # @param [Array] array An array of Streaming API events.
   def self.create_bulk_from_json(array)
     objects = array.map do |json|
       self.new(user_id: json[:source][:id],
@@ -12,7 +15,10 @@ class Favorite < ActiveRecord::Base
     self.import(objects, ignore: true)
   end
 
-  # This doesn't update Tweet#reactions_count.
+  # Unregisters favorite event in bulk from an array of Streaming API 'unfavorite' events.
+  # This method doesn't update Tweet#reactions_count.
+  #
+  # @param [Array] array An array of Streaming API events.
   def self.delete_bulk_from_json(array)
     array.each do |json|
       self.delete_all(user_id: json[:source][:id], tweet_id: json[:target_object][:id])
--- app/models/retweet.rb
+++ app/models/retweet.rb
@@ -2,7 +2,10 @@ class Retweet < ActiveRecord::Base
   belongs_to :tweet
   belongs_to :user
 
+  # Registers retweet event in bulk from an array of Streaming API messages.
   # This doesn't update Tweet#reactions_count.
+  #
+  # @param [Array] array An array of Streaming API messages.
   def self.create_bulk_from_json(array)
     objects = array.map do |json|
       self.new(id: json[:id],
@@ -13,7 +16,10 @@ class Retweet < ActiveRecord::Base
     self.import(objects, ignore: true)
   end
 
+  # Unregisters retweet events in bulk from array of Streaming API's delete events.
   # This doesn't update Tweet#reactions_count.
+  #
+  # @param [Array] array An array of Streaming API delete events.
   def self.delete_bulk_from_json(array)
     self.where(id: array.map {|json| json[:delete][:status][:id] }).delete_all
   end

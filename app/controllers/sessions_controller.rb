22057cb575105ab666b6c2cd668b9612a831574b
rhenium <rhenium@rhe.jp>
2014-09-03 07:03:18 +0900

    web: sessions#create: refactor

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
 
-    account = Account.register(user_id: auth["uid"],
-                               oauth_token: auth["credentials"]["token"],
-                               oauth_token_secret: auth["credentials"]["secret"])
+    account = Account.register(user_id: auth.uid,
+                               oauth_token: auth.credentials.token,
+                               oauth_token_secret: auth.credentials.secret)
     begin
       WorkerManager.update_account(account)
     rescue Aclog::Exceptions::WorkerConnectionError
@@ -12,10 +12,10 @@ class SessionsController < ApplicationController
 
     User.create_or_update_from_json(
       { id: account.user_id,
-        screen_name: auth["extra"]["raw_info"]["screen_name"],
-        name: auth["extra"]["raw_info"]["name"],
-        profile_image_url_https: auth["extra"]["raw_info"]["profile_image_url_https"],
-        protected: auth["extra"]["raw_info"]["protected"] })
+        screen_name: auth.extra.raw_info.screen_name,
+        name: auth.extra.raw_info.name,
+        profile_image_url_https: auth.extra.raw_info.profile_image_url_https,
+        protected: auth.extra.raw_info.protected })
 
     session[:user_id] = account.user_id
 

rhenium/aclog

commit 90d77ac45733c0916dd0c7a6c477589d6c79c82a

web: lazy loading user stats (sidebar)

app/assets/javascripts/parts/sidebar_user_stats.coffee
@@ -1,0 +1,24 @@
+Application.Parts.sidebar_user_stats = ->
+  loading = $(".user-stats .loading").show()
+  Application.Helpers.call_api "users/stats", { id: Application.Helpers.user_id() }, (json) ->
+    if json.registered
+      t = $("<ul />").addClass("records")
+      $(".user-stats").append(t)
+      addcol = (title, data, si) ->
+        d = $("<span />").addClass("data").text(data)
+        if si
+          d.append($("<span />").text(si))
+        line = $("<li />").append($("<span />").text(title)).append(d)
+        t.append(line)
+      l = [
+        ["Received", json.reactions_count],
+        ["Average", Math.round(json.reactions_count / json.tweets_count * 100) / 100],
+        ["Joined", json.since_join, "d ago"]
+      ]
+      l.forEach (l) -> addcol(l[0], l[1], l[2])
+    else
+      $(".user-stats").append(
+        $("<div />").addClass("alert").addClass("alert-aclog")
+          .text("@" + Application.Helpers.user_screen_name() + " は aclog に登録していません"))
+    loading.hide()
+    console.log json

app/assets/javascripts/_define_application.coffee
@@ -2,4 +2,4 @@ if window.Application is undefined
   window.Application =
     Views: {}
     Helpers: {}
-    Partials: {}
+    Parts: {}

app/assets/javascripts/_helpers.coffee.erb
@@ -2,6 +2,7 @@ Application.Helpers =
   controller: () -> $("body").data("controller")
   action: () -> $("body").data("action")
   user_id: () -> $("body").data("user-id")
+  user_screen_name: () -> $("body").data("user-screen-name")
   tweet_id: () -> $("body").data("tweet-id")
   rescue_profile_image: (range) ->
     $("img.twitter-icon", range).each () ->

app/assets/javascripts/application.coffee
@@ -9,13 +9,13 @@
 $ ->
   controller = $("body").data("controller")
   action = $("body").data("action")
-  partials = ($("body").data("partial") || "").split(" ")
+  parts = ($("body").data("parts") || "").split(" ")
 
   ac = Application.Views[controller]
   if ac
     ac["_"]?()
     ac["action"]?()
 
-  partials.forEach (par) ->
-    pa = Application.Partials[par]
+  parts.forEach (par) ->
+    pa = Application.Parts[par]
     pa?()

app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
     render json: filtered
   end
 
+  def i_stats
+    user = User.find(params[:id])
+    render json: user.stats.to_h
+  end
+
   private
   def require_user
     User.find(screen_name: params[:screen_name])

app/helpers/application_helper.rb
@@ -7,6 +7,21 @@ module ApplicationHelper
     end
   end
 
+  def register_view_part(name)
+    (@view_parts ||= []) << name
+  end
+
+  def meta_info
+    {
+      controller: controller.controller_path,
+      action: controller.action_name,
+      parts: @view_parts.try(:join, " "),
+      user_id: @user.try(:id),
+      user_screen_name: @user.try(:screen_name),
+      tweet_id: @tweet.try(:id)
+    }
+  end
+
   def link_to_with_active(name, options = {}, html_options = {}, &block)
     if current_page?(options)
       html_options[:class] = (html_options[:class].to_s + " active").strip

app/views/layouts/_base.html.haml
@@ -15,7 +15,7 @@
       %link{rel: "next", href: @next_url}
     - if @prev_url
       %link{rel: "prev", href: @prev_url}
-  %body{data: { controller: controller.controller_path, action: controller.action_name }}
+  %body{data: meta_info}
     %nav.navbar.navbar-static-top
       .container
         .nav.navbar-nav.navbar-left

app/views/shared/sidebar/parts/_user_stats.html.haml
@@ -1,20 +1,8 @@
+- register_view_part :sidebar_user_stats
 %p<
   = profile_image_tag user, :original, class: "img-rounded", width: 64, height: 64
 %p= "@" + user.screen_name
 %p<
   %a.aclogicon.aclogicon-twitter{href: user.twitter_url}
-- if user.registered?
-  %ul.records
-    %li<
-      %span> Received
-      %span.data>= user.stats.reactions_count
-    %li<
-      %span> Average
-      %span.data>= "%.2f" % (user.stats.reactions_count.to_f / user.stats.tweets_count)
-    %li<
-      %span> Joined
-      %span.data>
-        = user.stats.since_join
-        %span> d ago
-- else
-  .alert.alert-aclog @#{user.screen_name} は aclog に登録していません
+.user-stats
+  = loading_tag

commit 5669a33b41e9d1bcb97aa4c676d0725a45629001

web: remove unused console.log (caused by 90d77ac)

app/assets/javascripts/parts/sidebar_user_stats.coffee
@@ -21,4 +21,3 @@ Application.Parts.sidebar_user_stats = ->
         $("<div />").addClass("alert").addClass("alert-aclog")
           .text("@" + Application.Helpers.user_screen_name() + " は aclog に登録していません"))
     loading.hide()
-    console.log json
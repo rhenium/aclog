19db48201e7c022d761dd1aa99b97330a01a018c
rhenium <rhenium@rhe.jp>
2015-04-05 20:32:08 +0900

    web: no longer use twitter's widgets.js

--- app/controllers/concerns/security_headers.rb
+++ app/controllers/concerns/security_headers.rb
@@ -10,8 +10,7 @@ module SecurityHeaders
       "default-src" => "'self'",
       "img-src" => "'self' https://pbs.twimg.com https://abs.twimg.com",
       "style-src" => "'self' http://fonts.googleapis.com",
-      "font-src" => "'self' http://fonts.gstatic.com",
-      "frame-src" => "'self' http://platform.twitter.com"
+      "font-src" => "'self' http://fonts.gstatic.com"
     }
     policy_str = policy.map {|k, v| "#{k} #{v}" }.join("; ")
     response["Content-Security-Policy"] = policy_str

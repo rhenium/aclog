2f49271856809f18d04bfff3d3f75603358967e3
Kazuki Yamaguchi <k@rhe.jp>
2015-06-21 18:51:23 +0900

    DelayedJob: output log to STDOUT only when 'run'

--- config/initializers/delayed_jobs.rb
+++
@@ -1,4 +0,0 @@
-Delayed::Worker.logger = Logger.new(STDOUT)
-Delayed::Worker.logger.level =
-  Rails.env.production? ? Logger::INFO : Logger::DEBUG
-ActiveRecord::Base.logger = Delayed::Worker.logger
